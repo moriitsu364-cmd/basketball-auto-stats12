1766c359a10b676563306c0ab2bd52b2cd07215a
moriitsu364-cmd <moriitsu364@sgh-tsukuba.org>
2026-02-12 22:36:58 +0900

    Create init.py

---
+++ init.py
@@ -0,0 +1,14 @@
+"""ページモジュール"""
+from . import season_stats
+from . import player_stats
+from . import game_stats
+from . import compare
+from . import data_input
+
+__all__ = [
+    'season_stats',
+    'player_stats', 
+    'game_stats',
+    'compare',
+    'data_input'
+]
