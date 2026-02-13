abcf23f4d1a06c37b3bd605f573e0022a542debb
moriitsu364-cmd <moriitsu364@sgh-tsukuba.org>
2026-02-13 21:04:05 +0900

    Update __init__.py

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,5 +1,13 @@
-"""ページモジュール - 修正版"""
-# 循環インポートを避けるため、__init__.pyではインポートを行わない
+"""ページモジュール"""
+from . import season_stats
+from . import player_stats
+from . import game_stats
+from . import compare
+from . import data_input
+from . import team_info
+from . import opponent_stats
+from . import admin_settings
+
 __all__ = [
     'season_stats',
     'player_stats', 
